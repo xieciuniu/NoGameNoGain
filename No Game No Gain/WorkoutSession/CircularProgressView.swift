29cdc99eef09d91e2e4abe43e382b51e8603fca2
Uberto_W <96199775+xieciuniu@users.noreply.github.com>
2025-09-19 14:38:31 +0200

    add progress colors to progress circle

--- No Game No Gain/WorkoutSession/CircularProgressView.swift
+++ No Game No Gain/WorkoutSession/CircularProgressView.swift
@@ -9,32 +9,35 @@ import SwiftUI
 
 struct CircularProgressView: View {
     let progress: Double
+    var color: Color {
+        switch progress {
+        case 0.0 ..< 0.5:
+            return .yellow
+        case 0.5 ..< 1:
+            return .green
+        case 1:
+            return .blue
+        default:
+            return .red
+        }
+    }
+    
     var body: some View {
         ZStack {
             Circle()
-                .stroke(Color.green.opacity(0.5), lineWidth: 5)
-            if progress <= 1{
-                Circle()
-                    .trim(from: 0, to: progress)
-                    .stroke(
-                        Color.green,
-                        style: StrokeStyle(
+                .stroke(color.opacity(0.5), lineWidth: 5)
+            
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(
+                    color,
+                    style: StrokeStyle(
                             lineWidth: 5,
                             lineCap: .round
-                        )
                     )
-                    .rotationEffect(Angle(degrees: -90))
-                    .animation(.easeInOut(duration: 0.5), value: progress)
-            } else {
-                Circle()
-                    .stroke(
-                        Color.red,
-                        style: StrokeStyle(
-                            lineWidth: 5,
-                            lineCap: .round
-                        )
-                    )
-            }
+                )
+                .rotationEffect(Angle(degrees: -90))
+                .animation(.easeInOut(duration: 0.5), value: progress)
         }
     }
 }
